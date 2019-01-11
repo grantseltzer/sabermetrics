3f6ebfb2610b2e78562d100badd1ff9ac76f5117
grantseltzer <grantseltzer@gmail.com>
2019-01-11 01:15:26 -0500

    script for parsing csv files
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

---
+++ stat-award-correlation/separate-by-league.r
@@ -0,0 +1,39 @@
+# Read in data
+folder    <- "/home/rstudio/R/sabermetrics/stat-award-correlation/data/By-League"
+saveFolder <- "/home/rstudio/R/sabermetrics/stat-award-correlation/data/By-League/With-Stat-Rank"
+file_list <- list.files(path=folder, pattern="*.csv")
+
+for (i in 1:length(file_list)){
+  
+  data <- read.csv(paste(folder, file_list[i], sep=''))
+  
+  for (x in 2:nrow(data)) {
+    if (data[x,]$Rank == 1) {
+      al <- data[1:x-1,]
+      al_filename <- paste(saveFolder, "AL_",file_list[i], sep="")
+      write.csv(al, file = al_filename,row.names=FALSE)
+      
+      nl <- data[x:nrow(data),]
+      nl_filename <- paste(saveFolder, "NL_",file_list[i], sep="")
+      write.csv(nl, file = nl_filename,row.names=FALSE)
+      
+      break
+    }
+  }
+}
+
+
+
+#for(i in 2018) {
+  
+#  x <- data[data$Year == i,]
+#  al <- x[1:9,]
+#  al <- al[order(-al$WHIP),] 
+  
+#  al$WHIPRank<-NA
+#  for(i in 1:nrow(al)){
+#    al[i,]$WHIPRank = i
+#  }
+  
+#}
+
